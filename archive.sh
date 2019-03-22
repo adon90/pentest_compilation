c7d22ba6b9d34e509d2e102f32200e4f9a82a56c
root <root@localhost.localdomain>
2019-03-22 13:29:26 +0000

    tree

--- archive.sh
+++
@@ -1,5 +0,0 @@
-archive() {
-	
-	curl -s 'http://web.archive.org/cdx/search?url='$1'%2F&matchType=prefix&collapse=urlkey&output=json&fl=original%2Cmimetype%2Ctimestamp%2Cendtimestamp%2Cgroupcount%2Cuniqcount&filter=!statuscode%3A%5B45%5D..&limit=100000&_=1532513891577' --compressed | grep -Po "(?<=\[\").*?(?=\")"
-
-}
